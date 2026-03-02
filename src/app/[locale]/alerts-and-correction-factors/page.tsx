4ac8fe0abd6de424e201d56f57852bcc577b3ade
Miguel Angel <miguel.angel.angulo72@gmail.com>
2026-03-02 12:08:03 -0400

    Fixing dark and ligh mode in correction factors page

--- src/app/[locale]/alerts-and-correction-factors/page.tsx
+++ src/app/[locale]/alerts-and-correction-factors/page.tsx
@@ -3,34 +3,37 @@
 import { useThemeStore } from '@/store';
 import { useEffect } from 'react';
 import { AlertsList, FiltersBar, Tabs } from './components';
-import { useTranslations } from 'next-intl';
-import { useRouter, usePathname } from 'next/navigation';
+// import { useTranslations } from 'next-intl';
+// import { useRouter, usePathname } from 'next/navigation';
 
 export default function AlertsAndCorrectionFactorsPage() {
   const setTheme = useThemeStore((s) => s.setTheme);
-  const t = useTranslations('nowCastingPage');
-  const pathname = usePathname();
-  const router = useRouter();
+  // const t = useTranslations('nowCastingPage');
+  // const pathname = usePathname();
+  // const router = useRouter();
 
-  const switchLocale = (nextLocale: string) => {
-    const segments = pathname.split('/');
-    segments[1] = nextLocale;
-    router.push(segments.join('/'));
-  };
+  // const switchLocale = (nextLocale: string) => {
+  //   const segments = pathname.split('/');
+  //   segments[1] = nextLocale;
+  //   router.push(segments.join('/'));
+  // };
 
   useEffect(() => {
     setTheme('light');
+    return () => {
+      setTheme('dark');
+    };
   }, []);
 
   return (
     <main>
-      <div style={{ marginTop: 12 }}>
+      {/* <div style={{ marginTop: 12 }}>
         <button onClick={() => switchLocale('en')}>EN</button>
         <button onClick={() => switchLocale('es')}>ES</button>
         <button onClick={() => switchLocale('fr')}>FR</button>
       </div>
 
-      <h2>{t('message_loading')}</h2>
+      <h2>{t('message_loading')}</h2> */}
 
       <div className="flex flex-col h-full bg-background text-foreground">
         <Tabs />
